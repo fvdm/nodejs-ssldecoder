4e7a52d40eb87bbc72342ea9b7957e77b6db9341
Franklin van de Meent <fr@nkl.in>
2016-04-26 23:30:12 +0200

    Test: added env vars Timeout and Endpoint

--- test.js
+++ test.js
@@ -1,10 +1,18 @@
 var dotest = require ('dotest');
 var app = require ('./');
 
-var ssl = app ();
+var config = {
+  timeout: process.env.Timeout || 20000,
+  endpoint: process.env.Endpoint || null
+};
+
+var ssl = app (config);
+
 
 dotest.add ('Module', function (test) {
   test ()
+    .info ('config.timeout:   ' + config.timeout)
+    .info ('config.endpoint:  ' + (config.endpoint ? 'custom' : 'live'))
     .isFunction ('fail', 'exports', app)
     .isObject ('fail', 'interface', ssl)
     .isFunction ('fail', '.host', ssl && ssl.host)
