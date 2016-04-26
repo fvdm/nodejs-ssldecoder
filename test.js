c79f10035dfa3703d4ff25da9da47cbeb3db7985
Franklin van de Meent <fr@nkl.in>
2016-04-26 14:53:53 +0200

    Test: check module interface

--- test.js
+++ test.js
@@ -1,4 +1,14 @@
 var dotest = require ('dotest');
+var app = require ('./');
 
-dotest.log ('fail', 'No test script');
-dotest.exit ();
+var ssl = app ();
+
+dotest.add ('Module', function (test) {
+  test ()
+    .isFunction ('fail', 'exports', app)
+    .isObject ('fail', 'interface', ssl)
+    .isFunction ('fail', '.host', ssl && ssl.host)
+    .done ();
+});
+
+dotest.run ();
