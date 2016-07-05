da02f479ea3c570a1a85782e124b95626ba22f37
Franklin van de Meent <fr@nkl.in>
2016-07-06 00:06:28 +0200

    Test(runner): Replaced CodeClimate with Coveralls

--- test.sh
+++ test.sh
@@ -2,13 +2,10 @@
 result=0
 
 eslint *.js || result=1
+istanbul cover test.js || result=1
 
-if [ "$TRAVIS_BRANCH" == "master" ] && [ "$CODECLIMATE_REPO_TOKEN" != "" ]; then
-  istanbul cover test.js --print none --report lcovonly || result=1
-  [ "$result" -eq "0" ] && codeclimate-test-reporter < ./coverage/lcov.info || result=1
-else
-  node test.js || result=1
+if [ "$TRAVIS" == "true" ]; then
+  cat ./coverage/lcov.info | ./node_modules/coveralls/bin/coveralls.js || result=1
 fi
 
 exit $result
-
